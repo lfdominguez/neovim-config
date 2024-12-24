3bf72243fb9b4087f30d4589cc219c0a84628ec3
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2024-12-24 12:04:11 -0300

    autocomplete crates names

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -3,6 +3,14 @@ return {
     tag = 'stable',
     event = { "BufRead Cargo.toml" },
     config = function()
-        require('crates').setup()
+        require('crates').setup({
+            completion = {
+                crates = {
+                    enabled = true,
+                    max_results = 8,
+                    min_chars = 3,
+                },
+            },
+        })
     end,
 }
